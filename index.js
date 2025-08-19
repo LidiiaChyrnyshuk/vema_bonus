88cdf82d045188776eb886510a910d37f04ed71d
LidiiaChyrnyshuk <l.mandych@gmail.com>
2025-08-19 16:54:23 +0200

    change animation

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	const hoursEl = document.getElementById("hours");
 	const minutesEl = document.getElementById("minutes");
 	const secondsEl = document.getElementById("seconds");
-	const hundredthsEl = document.getElementById("hundredths");
 
 	// Встановлюємо час завершення: зараз + 10 хв
-	const endTime = Date.now() + 3 * 60 * 1000;
+	const endTime = Date.now() + 1 * 60 * 1000;
 
 	const interval = setInterval(() => {
 		const now = Date.now();
@@ -18,11 +17,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		if (timeLeft <= 0) {
 			clearInterval(interval);
-			daysEl.textContent = "00";
+/* 			daysEl.textContent = "00";
 			hoursEl.textContent = "00";
 			minutesEl.textContent = "00";
 			secondsEl.textContent = "00";
-			hundredthsEl.textContent = "00";
+			hundredthsEl.textContent = "00"; */
+			window.location.href = "https://www.vemabet.com/en/";
 			return;
 		}
 
@@ -32,12 +32,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		);
 		const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
 		const seconds = Math.floor((timeLeft / 1000) % 60);
-		const hundredths = Math.floor((timeLeft % 1000) / 10);
 
 		daysEl.textContent = addZero(days);
 		hoursEl.textContent = addZero(hours);
 		minutesEl.textContent = addZero(minutes);
 		secondsEl.textContent = addZero(seconds);
-		hundredthsEl.textContent = addZero(hundredths);
 	}, 33);
 });
